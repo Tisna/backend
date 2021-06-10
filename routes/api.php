149ff2213865138a5cf54babf40b6a712a12b33f
Wayan Tisna <wayantisna74@gmail.com>
2021-06-10 14:06:21 +0800

    add sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/auth/register', [\App\Http\Controllers\Auth\AuthController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/login', [\App\Http\Controllers\Auth\AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', [\App\Http\Controllers\UserController::class, 'user']);
+
+    Route::post('/auth/logout', [\App\Http\Controllers\Auth\AuthController::class, 'logout']);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
-    resolve: {
-        alias: {
-            '@': path.resolve('resources/js'),
-        },
+  resolve: {
+    alias: {
+      '@': path.resolve('resources/js'),
     },
+  },
+  plugins: [
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+      threshold: 10240,
+      minRatio: Number.MAX_SAFE_INTEGER, // Compress everything
+    }),
+    // new CompressionPlugin({
+    //   filename: "[path][base].br",
+    //   algorithm: "brotliCompress",
+    //   test: /\.(js|css|html|svg)$/,
+    //   compressionOptions: {
+    //     params: {
+    //       [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+    //     },
+    //   },
+    //   threshold: 10240,
+    //   minRatio: 0.8,
+    // }),
+  ],
 };
